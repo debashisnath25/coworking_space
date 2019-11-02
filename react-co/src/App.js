137dec82cf4f4f778272cacd8f7ccd19f5f1785f
Debashis Nath <debashisnath1992@gmail.com>
2019-11-02 18:05:54 +0530

    pushh

--- react-co/src/App.js
+++ react-co/src/App.js
@@ -7,13 +7,14 @@ import Footer from './components/common/Footer';
 
 // Other components to render
 import Homepage from './pages/Homepage';
-
+import About_us from './pages/About_us';
 function App() {
   return (
     <Router>
       <div>
         <Header />
         <Route exact path="/" component={Homepage} />
+        <Route exact path="/about_us" component={About_us} />
         <Footer />
       </div>
      </Router>
