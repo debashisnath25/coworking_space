257d9349b4f6bf4b642a9786268870298afa94b2
Pipon77 <pipondas77@gmail.com>
2020-01-23 17:43:46 +0530

    fsrgr
    dgdr

--- html/index.php
+++ html/index.php
@@ -626,13 +626,11 @@
                 <div class="col-12">
                     <div class="all-title">
                         <h3 class="sec-title">
-                            fitness programs
+                            How It Works
                         </h3>
                         <svg class="title-sep">
                             <path fill-rule="evenodd" d="M32.000,13.000 L32.000,7.000 L35.000,7.000 L35.000,13.000 L32.000,13.000 ZM24.000,4.000 L27.000,4.000 L27.000,16.000 L24.000,16.000 L24.000,4.000 ZM16.000,-0.000 L19.000,-0.000 L19.000,20.000 L16.000,20.000 L16.000,-0.000 ZM8.000,4.000 L11.000,4.000 L11.000,16.000 L8.000,16.000 L8.000,4.000 ZM-0.000,7.000 L3.000,7.000 L3.000,13.000 L-0.000,13.000 L-0.000,7.000 Z"></path>
                         </svg>
-                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor penatibus
-                            ean massa. Cum sociis natoqu penatibus et magnis dis parturient montes.</p>
                     </div>
                 </div>
             </div>
@@ -644,7 +642,7 @@
                         </div>
                         <h5>
                             <span class="c-theme">1. </span> Choose a Category</h5>
-                        <p class="mb-0">Lorem ipsum dolor sit amet, consec rcqui adipiscing elit socada como ligaui.</p>
+                        <p class="mb-0">Search the market, shortlist options and request viewing and confirm viewing</p>
                     </div>
                 </div>
                 <div class="col-lg-4 col-md-6 col-12 mb-30">
@@ -654,7 +652,7 @@
                         </div>
                         <h5>
                             <span class="c-theme">2. </span> Find What You Want</h5>
-                        <p class="mb-0">Lorem ipsum dolor sit amet, consec rcqui adipiscing elit socada como ligaui.</p>
+                        <p class="mb-0">Choose your perfect and we'll help negotiate the best deal on anything from start to finish</p>
                     </div>
                 </div>
                 <div class="col-lg-4 col-md-6 col-12 mb-30">
@@ -664,7 +662,7 @@
                         </div>
                         <h5>
                             <span class="c-theme">3. </span> go Out &amp; Explore</h5>
-                        <p class="mb-0">Lorem ipsum dolor sit amet, consec rcqui adipiscing elit socada como ligaui.</p>
+                        <p class="mb-0">Flexible to change the workspace as your growth on demand.we're there to advise</p>
                     </div>
                 </div>
             </div>
